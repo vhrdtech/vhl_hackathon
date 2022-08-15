69fb9fb9b282193360584f48e8172070b8151e13
Roman Isaikin <romix.lab@gmail.com>
2022-08-15 14:05:09 +0200

    02: Bring up color OLED.

--- 02_can_gps_lora/can_gps_lora/src/main.rs
+++ 02_can_gps_lora/can_gps_lora/src/main.rs
@@ -3,17 +3,20 @@
 
 extern crate panic_rtt_target;
 
+use cfg_if::cfg_if;
 use cortex_m::asm::delay;
 use embedded_graphics::Drawable;
-use embedded_graphics::geometry::Point;
+use embedded_graphics::geometry::{OriginDimensions, Point};
 use embedded_graphics::image::{Image, ImageRaw};
-use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::pixelcolor::{BinaryColor, Rgb565};
+use embedded_graphics::transform::Transform;
 use heapless::{consts::U8, spsc};
 use nb::block;
 use rtt_target::{rprint, rprintln};
 use ssd1306::{I2CDisplayInterface, Ssd1306};
 use ssd1306::mode::DisplayConfig;
 use ssd1306::prelude::{DisplayRotation, DisplaySize128x64};
+use ssd1331::DisplayRotation::Rotate0;
 use stm32l4xx_hal::{i2c, pac::{self, LPUART1}, prelude::*, serial::{self, Config, Serial}};
 use stm32l4xx_hal::delay::Delay;
 use stm32l4xx_hal::gpio::{Floating, H8, Input, L8, Output, Pin, PushPull};
@@ -21,6 +24,7 @@ use stm32l4xx_hal::hal::blocking::delay::DelayMs;
 use stm32l4xx_hal::i2c::I2c;
 use stm32l4xx_hal::spi::Spi;
 use sx127x_lora::MODE;
+use tinybmp::Bmp;
 
 pub const SYSCLK: u32 = 64_000_000;
 
@@ -58,6 +62,7 @@ const APP: () = {
         let mut gpioa = p.GPIOA.split(&mut rcc.ahb2);
         let mut gpiob = p.GPIOB.split(&mut rcc.ahb2);
         let mut gpioc = p.GPIOC.split(&mut rcc.ahb2);
+        let mut gpiod = p.GPIOD.split(&mut rcc.ahb2);
 
         let mut on_off = gpioc.pc4.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
         let tx_pin = gpioc
@@ -121,50 +126,99 @@ const APP: () = {
             Err(e) => {
                 loop {
                     rprintln!("Lora error: {:?}", e);
-                    cortex_m::asm::delay(10_000);
+                    delay.delay_ms(250u8);
                 }
             }
         };
 
-        let mut scl =
-            gpiob
-                .pb6
-                .into_alternate_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
-        scl.internal_pull_up(&mut gpiob.pupdr, true);
+        cfg_if! {
+            if #[cfg(feature = "oled_bw_ssd1306")] {
+                let mut scl =
+                    gpiob
+                        .pb6
+                        .into_alternate_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+                scl.internal_pull_up(&mut gpiob.pupdr, true);
 
-        let mut sda =
-            gpiob
-                .pb7
-                .into_alternate_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
-        sda.internal_pull_up(&mut gpiob.pupdr, true);
+                let mut sda =
+                    gpiob
+                        .pb7
+                        .into_alternate_open_drain(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrl);
+                sda.internal_pull_up(&mut gpiob.pupdr, true);
 
-        let mut i2c = I2c::i2c1(
-            p.I2C1,
-            (scl, sda),
-            i2c::Config::new(100.kHz(), clocks),
-            &mut rcc.apb1r1,
-        );
+                let mut i2c = I2c::i2c1(
+                    p.I2C1,
+                    (scl, sda),
+                    i2c::Config::new(100.kHz(), clocks),
+                    &mut rcc.apb1r1,
+                );
 
-        rprintln!("i2c scan...");
-        for i in 0..=127 {
-            let mut buf = [0u8];
-            if i2c.read(i, &mut buf).is_ok() {
-                rprintln!("{:02x} found", i);
+                rprintln!("i2c scan...");
+                for i in 0..=127 {
+                    let mut buf = [0u8];
+                    if i2c.read(i, &mut buf).is_ok() {
+                        rprintln!("{:02x} found", i);
+                    }
+                }
+
+                let interface = I2CDisplayInterface::new(i2c);
+                let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+                    .into_buffered_graphics_mode();
+                display.init().unwrap();
+
+                let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("../rust.raw"), 64);
+                let im = Image::new(&raw, Point::new(32, 0));
+                im.draw(&mut display).unwrap();
+                display.flush().unwrap();
+            } else if #[cfg(feature = "oled_color_ssd1331")] {
+                let sck = gpioc
+                    .pc10
+                    .into_alternate(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrh);
+                let miso = gpioc
+                    .pc11
+                    .into_alternate(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrh);
+                let mosi = gpioc
+                    .pc12
+                    .into_alternate(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrh);
+                let mut spi3 = Spi::spi3(
+                    p.SPI3,
+                    (sck, miso, mosi),
+                    MODE,
+                    // 1.MHz(),
+                    100.kHz(),
+                    clocks,
+                    &mut rcc.apb1r1,
+                );
+                let mut oled_reset = gpiod.pd2.into_push_pull_output(&mut gpiod.moder, &mut gpiod.otyper);
+                oled_reset.set_low();
+                delay.delay_ms(100u8);
+                oled_reset.set_high();
+                let oled_dc = gpioc.pc9.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+
+                let mut display = ssd1331::Ssd1331::new(spi3, oled_dc, Rotate0);
+                display.init().unwrap();
+                // display.flush().unwrap();
+
+                let (w, h) = display.dimensions();
+
+                let bmp = tinybmp::Bmp::from_slice(include_bytes!("../rust_pride.bmp"))
+                    .expect("Failed to load BMP image");
+
+                let im: Image<Bmp<Rgb565>> = Image::new(&bmp, Point::zero());
+
+                // Position image in the center of the display
+                let moved = im.translate(Point::new(
+                    (w as u32 - bmp.size().width) as i32 / 2,
+                    (h as u32 - bmp.size().height) as i32 / 2,
+                ));
+
+                moved.draw(&mut display).unwrap();
+
+                display.flush().unwrap();
             }
         }
 
-        let interface = I2CDisplayInterface::new(i2c);
-        let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-            .into_buffered_graphics_mode();
-        display.init().unwrap();
 
-        let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("../rust.raw"), 64);
 
-        let im = Image::new(&raw, Point::new(32, 0));
-
-        im.draw(&mut display).unwrap();
-
-        display.flush().unwrap();
 
         let (tx, rx) = serial.split();
         let (rx_prod, rx_cons) = RX_QUEUE.split();
