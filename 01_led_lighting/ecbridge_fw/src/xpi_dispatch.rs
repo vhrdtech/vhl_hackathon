e83b0edc11c9071e8b6d259af5466bb2efd7a67b
Roman Isaikin <romix.lab@gmail.com>
2022-10-10 18:24:43 +0200

    01: ecbridge_fw: Refactor xpi_dispatch.

--- 01_led_lighting/ecbridge_fw/src/xpi_dispatch.rs
+++ 01_led_lighting/ecbridge_fw/src/xpi_dispatch.rs
@@ -1,16 +1,17 @@
-use core::cmp::max;
-use rtt_target::{rprintln};
-use vhl_cg::point::Point;
-use vhl_stdlib::serdes::buf::{Buf, BufMut};
 use crate::{log_error, log_info, log_trace, log_warn};
-use vhl_stdlib::serdes::buf::Error as BufError;
-use vhl_stdlib::serdes::{NibbleBuf, NibbleBufMut, SerDesSize, SerializeVlu4};
-use vhl_stdlib::serdes::vlu4::{Vlu32, Vlu4VecIter};
-use xpi::event_kind::{XpiEventDiscriminant, XpiGenericEventKind};
-use xpi::xwfd::{NodeId, SerialUriIter, NodeSet, EventKind, EventBuilder, MultiUriFlatIter};
-use xpi::error::XpiError;
-use xpi::xwfd;
+use core::cmp::max;
+use rtt_target::rprintln;
+use vhl_cg::point::Point;
 use vhl_stdlib::discrete::U4;
+use vhl_stdlib::serdes::buf::Error as BufError;
+use vhl_stdlib::serdes::buf::{Buf, BufMut};
+use vhl_stdlib::serdes::vlu4::{Vlu32, Vlu4Vec, Vlu4VecIter};
+use vhl_stdlib::serdes::{NibbleBuf, NibbleBufMut, SerDesSize, SerializeVlu4};
+use xpi::error::XpiError;
+use xpi::event_kind::{XpiEventDiscriminant, XpiGenericEventKind};
+use xpi::xwfd;
+use xpi::xwfd::event::EventBuilderKindState;
+use xpi::xwfd::{EventBuilder, EventKind, MultiUriFlatIter, NodeId, NodeSet, SerialUriIter};
 
 pub type DispatcherContext<'c> = crate::app::link_process::Context<'c>;
 
@@ -44,10 +45,13 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
     let mut resource_set_execute_uri_iter = ev.resource_set.flat_iter();
     let ev_kind = ev.kind.discriminant();
     for _ in 0..MAX_REPLY_BATCHES {
-        let mut reply_lookahead: [Option<ReplySizeHint>; MAX_REPLY_BATCH_LEN] = [None; MAX_REPLY_BATCH_LEN];
+        let mut reply_lookahead: [Option<ReplySizeHint>; MAX_REPLY_BATCH_LEN] =
+            [None; MAX_REPLY_BATCH_LEN];
         let mut run_out_of_requests = false;
         let mut batch_len = 0;
-        let mut reply_nibbles_left = (REPLY_MTU - /* frame sync overhead */5) * 2 - /*header*/10 - /*tail*/2 - /*spare*/10;
+        let mut immediate_replies = 0;
+        let mut reply_nibbles_left =
+            (REPLY_MTU - /* frame sync overhead */5) * 2 - /*header*/10 - /*tail*/2 - /*spare*/10;
         for idx in 0..MAX_REPLY_BATCH_LEN {
             match resource_set_lookahead_uri_iter.peek() {
                 Some(uri) => {
@@ -60,6 +64,7 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
                                 let _ = resource_set_lookahead_uri_iter.next();
                                 reply_lookahead[idx] = Some(hint);
                                 batch_len += 1;
+                                immediate_replies += 1;
                                 reply_nibbles_left -= upper_bound;
                             } else {
                                 // reply won't fit, stop and put it into next batch
@@ -73,7 +78,6 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
                             batch_len += 1;
                         }
                     }
-
                 }
                 None => {
                     run_out_of_requests = true;
@@ -88,94 +92,50 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
                 self_node_id,
                 ev.request_id,
                 ev.priority,
-                U4::new(15).unwrap()
-            ).unwrap();
-            let reply_builder = reply_builder.build_node_set_with(|mut nwr| {
-                let node_set = NodeSet::Unicast(ev.source);
-                node_set.ser_vlu4(&mut nwr)?;
-                Ok((node_set.ser_header(), nwr))
-            }).unwrap();
-            let reply_builder = reply_builder.build_resource_set_with(|mut nwr| {
-                let resource_set = ev.resource_set.clone();
-                resource_set.ser_vlu4(&mut nwr)?;
-                Ok((resource_set.ser_header(), nwr))
-            }).unwrap();
+                U4::new(15).unwrap(),
+            )
+            .unwrap();
+            let reply_builder = reply_builder
+                .build_node_set_with(|mut nwr| {
+                    let node_set = NodeSet::Unicast(ev.source);
+                    node_set.ser_vlu4(&mut nwr)?;
+                    Ok((node_set.ser_header(), nwr))
+                })
+                .unwrap();
+            let reply_builder = reply_builder
+                .build_resource_set_with(|mut nwr| {
+                    let resource_set = ev.resource_set.clone();
+                    resource_set.ser_vlu4(&mut nwr)?;
+                    Ok((resource_set.ser_header(), nwr))
+                })
+                .unwrap();
             // dispatch_batch(
             //     ctx,
             //     &mut resource_set_execute_uri_iter,
             //     &lookahead_reply_size[..batch_len],
             //     &ev.kind
             // );
-            match &ev.kind {
-                EventKind::Call { args_set } => {
-                    let nwr = reply_builder.build_kind_with(|nwr| {
-                        let mut vb = nwr.put_vec::<Result<NibbleBuf, XpiError>>();
-                        let mut args_set_iter = args_set.iter();
-                        for reply_size_hint in &reply_lookahead[..batch_len] {
-                            let uri = resource_set_execute_uri_iter.next().expect("");
-                            match reply_size_hint {
-                                Some(ReplySizeHint::Sync { raw_size, preliminary_result, ..}) => {
-                                    match preliminary_result {
-                                        Ok(_) => {
-                                            match args_set_iter.next() {
-                                                Some(args_nrd) => {
-                                                    vb.put_result_nib_slice_with(*raw_size, |result_nwr| {
-                                                        dispatch_call(
-                                                            uri.clone(),
-                                                            args_nrd,
-                                                            result_nwr,
-                                                        ).map(|_| ()).map_err(|e| {
-                                                            log_error!(=>T, "dispatch error: {:?}", e);
-                                                            e
-                                                        })
-                                                    })?;
-                                                }
-                                                None => {
-                                                    log_error!(=>T, "No args provided for {}", uri);
-                                                    vb.put(&Err(XpiError::NoArgumentsProvided))?;
-                                                }
-                                            }
-                                        }
-                                        Err(e) => {
-                                            vb.put(&Err(e.clone()))?;
-                                            continue;
-                                        }
-                                    }
-                                }
-                                Some(ReplySizeHint::Async) => {
-                                    match args_set_iter.next() {
-                                        Some(args_nrd) => {
-                                            match dispatch_call(uri.clone(), args_nrd, &mut NibbleBufMut::new_all(&mut [])) {
-                                                Ok(_) => { log_trace!(=>T, "async call spawned"); }
-                                                Err(e) => { log_error!(=>T, "dispatch error: {:?}", e); }
-                                            }
-                                        }
-                                        None => {
-                                            log_error!(=>T, "No args provided for {}", uri);
-                                            vb.put(&Err(XpiError::NoArgumentsProvided))?;
-                                        }
-                                    }
-                                }
-                                None => {
-                                    break; // shouldn't be reached, if batch_len is correct
-                                }
-                            }
-                        }
-
-                        let nwr = vb.finish()?;
-                        Ok((XpiEventDiscriminant::CallResults, nwr))
-                    })?;
-                    log_trace!(=>T, "XpiReply {}, free space left={} expected:{}", nwr, nwr.nibbles_left(), reply_nibbles_left);
-                    let (_, len, _) = nwr.finish();
-                    log_trace!(=>T, "commit {}", len);
-                    eth_wgr.commit(len);
-                    rtic::pend(stm32h7xx_hal::pac::Interrupt::ETH);
-                }
+            let nwr = match &ev.kind {
+                EventKind::Call { args_set } => dispatch_call_set(
+                    &mut resource_set_execute_uri_iter,
+                    &reply_lookahead[..batch_len],
+                    reply_builder,
+                    args_set,
+                )?,
                 u => {
                     rprintln!(=>T, "Unsupported: {}", u);
+                    continue; // TODO: is it correct?
                 }
+            };
+            if immediate_replies == 0 {
+                log_trace!(=>T, "Only async replies in a batch, not committing.");
+            } else {
+                log_trace!(=>T, "XpiReply {}, free space left={} expected:{}", nwr, nwr.nibbles_left(), reply_nibbles_left);
+                let (_, len, _) = nwr.finish();
+                log_trace!(=>T, "commit {}", len);
+                eth_wgr.commit(len);
+                rtic::pend(stm32h7xx_hal::pac::Interrupt::ETH);
             }
-
         }
         if run_out_of_requests || batch_len == 0 {
             break;
@@ -186,6 +146,75 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
     }
 
     Ok(())
+}
+
+fn dispatch_call_set<'i>(
+    resource_set_execute_uri_iter: &mut MultiUriFlatIter,
+    reply_lookahead: &[Option<ReplySizeHint>],
+    reply_builder: EventBuilderKindState<'i>,
+    args_set: &Vlu4Vec<NibbleBuf>,
+) -> Result<NibbleBufMut<'i>, XpiError> {
+    let nwr = reply_builder.build_kind_with(|nwr| {
+        let mut vb = nwr.put_vec::<Result<NibbleBuf, XpiError>>();
+        let mut args_set_iter = args_set.iter();
+        for reply_size_hint in reply_lookahead {
+            let uri = resource_set_execute_uri_iter.next().expect("");
+            match reply_size_hint {
+                Some(ReplySizeHint::Sync {
+                    raw_size,
+                    preliminary_result,
+                    ..
+                }) => match preliminary_result {
+                    Ok(_) => match args_set_iter.next() {
+                        Some(args_nrd) => {
+                            vb.put_result_nib_slice_with(*raw_size, |result_nwr| {
+                                dispatch_call(uri.clone(), args_nrd, result_nwr)
+                                    .map(|_| ())
+                                    .map_err(|e| {
+                                        log_error!(=>T, "dispatch error: {:?}", e);
+                                        e
+                                    })
+                            })?;
+                        }
+                        None => {
+                            log_error!(=>T, "No args provided for {}", uri);
+                            vb.put(&Err(XpiError::NoArgumentsProvided))?;
+                        }
+                    },
+                    Err(e) => {
+                        vb.put(&Err(e.clone()))?;
+                        continue;
+                    }
+                },
+                Some(ReplySizeHint::Async) => match args_set_iter.next() {
+                    Some(args_nrd) => {
+                        match dispatch_call(
+                            uri.clone(),
+                            args_nrd,
+                            &mut NibbleBufMut::new_all(&mut []),
+                        ) {
+                            Ok(_) => {
+                                log_trace!(=>T, "async call spawned");
+                            }
+                            Err(e) => {
+                                log_error!(=>T, "dispatch error: {:?}", e);
+                            }
+                        }
+                    }
+                    None => {
+                        log_error!(=>T, "No args provided for {}", uri);
+                        vb.put(&Err(XpiError::NoArgumentsProvided))?;
+                    }
+                },
+                None => {
+                    break; // shouldn't be reached, if batch_len is correct
+                }
+            }
+        }
+        let nwr = vb.finish()?;
+        Ok((XpiEventDiscriminant::CallResults, nwr))
+    })?;
+    Ok(nwr)
 }
 
 // fn dispatch_batch(
@@ -209,15 +238,14 @@ pub fn xpi_dispatch(ctx: &mut DispatcherContext, ev: &xwfd::Event) -> Result<(),
 fn dispatch_call(
     mut uri: SerialUriIter<Vlu4VecIter<Vlu32>>,
     mut args_nrd: NibbleBuf,
-    result_nwr: &mut NibbleBufMut
-) -> Result<(), XpiError>
-{
+    result_nwr: &mut NibbleBufMut,
+) -> Result<(), XpiError> {
     log_info!(=>T, "dispatch_call({})", uri);
     match uri.next() {
         None => {
             log_error!(=>T, "Expected root level");
             return Err(XpiError::BadUri);
-        },
+        }
         id @ Some(0 | 1) => {
             log_error!(=>T, "Resource /{:?} is not a method", id);
             Err(XpiError::NotAMethod)
@@ -232,7 +260,8 @@ fn dispatch_call(
         Some(5) => {
             let p1: Point = args_nrd.des_vlu4()?;
             let p2: Point = args_nrd.des_vlu4()?;
-            if !args_nrd.is_at_end() { // TODO: remove this as semver compatible newer versions can contain more data
+            if !args_nrd.is_at_end() {
+                // TODO: remove this as semver compatible newer versions can contain more data
                 log_warn!(=>T, "Unused {} nib left after deserializing arguments", args_nrd.nibbles_left());
             }
             let r = crate::sync(p1, p2);
@@ -271,7 +300,7 @@ enum ReplySizeHint {
     },
     /// Request is processed asynchronously, cannot reply right away. Appropriate task is spawned and
     /// is responsible to submit request
-    Async
+    Async,
 }
 
 impl ReplySizeHint {
@@ -286,42 +315,45 @@ impl ReplySizeHint {
 
 ///
 /// TODO: use proper max() or calculate in advance during code gen
-fn reply_size_hint(mut uri: SerialUriIter<Vlu4VecIter<Vlu32>>, event_kind: XpiEventDiscriminant) -> ReplySizeHint {
+fn reply_size_hint(
+    mut uri: SerialUriIter<Vlu4VecIter<Vlu32>>,
+    event_kind: XpiEventDiscriminant,
+) -> ReplySizeHint {
     log_trace!(=>T, "reply_size_hint({})", uri);
     use XpiEventDiscriminant::*;
     let not_supported = Err(XpiError::OperationNotSupported);
-    let not_supported = ReplySizeHint::sync(not_supported.len_nibbles(), SerDesSize::Sized(0), not_supported);
+    let not_supported = ReplySizeHint::sync(
+        not_supported.len_nibbles(),
+        SerDesSize::Sized(0),
+        not_supported,
+    );
     let bad_uri = Err(XpiError::BadUri);
     let bad_uri = ReplySizeHint::sync(bad_uri.len_nibbles(), SerDesSize::Sized(0), bad_uri);
     match uri.next() {
         // /main
-        None => {
-            match event_kind {
-                _ => not_supported,
-            }
+        None => match event_kind {
+            _ => not_supported,
         },
         Some(5) => {
             match uri.next() {
                 // dispatch /main/sync
-                None => {
-                    match event_kind {
-                        Call => ReplySizeHint::sync(SerDesSize::Sized(8 + 3), SerDesSize::Sized(8), Ok(())),
-                        _ => not_supported
+                None => match event_kind {
+                    Call => {
+                        ReplySizeHint::sync(SerDesSize::Sized(8 + 3), SerDesSize::Sized(8), Ok(()))
                     }
-                }
+                    _ => not_supported,
+                },
                 // /main/sync : has no child resources
                 Some(_) => bad_uri,
             }
-        },
+        }
         Some(6) => {
             match uri.next() {
                 // dispatch /main/async
-                None => {
-                    match event_kind {
-                        Call => ReplySizeHint::Async,
-                        _ => not_supported
-                    }
-                }
+                None => match event_kind {
+                    Call => ReplySizeHint::Async,
+                    _ => not_supported,
+                },
                 // /main/async : has no child resources
                 Some(_) => bad_uri,
             }
